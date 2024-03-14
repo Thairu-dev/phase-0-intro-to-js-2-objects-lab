d2b596c0a20f667394771cfd2e82e45c856c909f
Joseph_Thairu <joejoement@gmail.com>
2024-03-14 18:24:47 +0300

    objects lab complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+const employee = {name: "joe", streetAddress: "0011 nairobi" }
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {...employee,[key]:value}
+
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const employee1 = {...employee, key}
+    return {employee1, delete:employee1.key}
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    employee[key] = undefined
+    return employee
+        
+}
